8921365dba01e1fce9b3d143cbd397a03da19c90
오태현 <jeffrey@kai-i.com>
2024-01-25 12:14:41 +0900

    NumberLineJumps Done

--- hackerRank/implementation/NumberLineJumps.java
+++ hackerRank/implementation/NumberLineJumps.java
@@ -2,14 +2,35 @@ package hackerRank.implementation;
 
 public class NumberLineJumps {
     public static void main(String[] args) {
-
+        int x1 = 43;
+        int v1 = 2;
+        int x2 = 70;
+        int v2 = 2;
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
     }
 
     public static String kangaroo(int x1, int v1, int x2, int v2) {
         // Write your code here
-        // x1 < x2, v1 > v2 YES
-        // x1 < x2, v1 <= v2 NO
+        String yes = "YES";
+        String no = "NO";
 
-        return null;
+        if (x1 == x2) {
+            if (v1 != v2) return no;
+        } else {
+            if (v1 == v2) return no;
+
+            int mod = Math.abs((x1 - x2) % (v1 - v2));
+
+            if (x1 < x2) {
+                if (v1 < v2) return no;
+            } else {
+                if (v1 > v2) return no;
+            }
+            if (mod != 0) return no;
+
+        }
+        return yes;
     }
+
 }
